508e0c5a665210003889d0915b937b4f58bbf9a9
kowshi-ka <106224716+kowshi-ka@users.noreply.github.com>
2022-09-18 22:40:53 +0530

    get the input from user until the string is palindrome

---
+++ String_palindrome.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    String s="";
+	    while(sc.hasNext()){
+	         s=sc.next();
+	        String h="";
+	        int l=s.length();
+	        for(int i=l-1;i>=0;i--){
+	            h+=s.charAt(i);
+	        }
+	        if(s.equals(h)){
+	            break;
+	        }else{
+	            continue;
+	        }
+	    }
+	    System.out.println("Palindrome found "+s);
+	}
+}
